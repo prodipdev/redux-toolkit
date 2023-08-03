48697236c0bf16e83d2cd7378356617440fc3907
prodipdev <prodipink@gmail.com>
2023-08-03 22:17:53 +0600

    Added fetch methods

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
-// import postReducer from "../features/posts/postSlice";
+import postReducer from "../features/posts/postSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
-    // posts: postReducer,
+    posts: postReducer,
   },
 });
 
--- src/features/posts/PostView.jsx
+++ src/features/posts/PostView.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "./postSlice";
+
 const PostView = () => {
-  return <div></div>;
+  const { isLoading, posts, error } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Posts View</h1>
+      {isLoading && <h3>Loading...</h3>}
+      {error && <h3>{error}</h3>}
+      <section>
+        {posts &&
+          posts.map((post) => (
+            <article key={post.title}>
+              <h5>{post.title}</h5> <p>{post.body}</p>
+            </article>
+          ))}
+      </section>
+    </div>
+  );
 };
 
 export default PostView;
